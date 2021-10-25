b7802c0ffc3563a372f271abafee72947845b247
mckinseyleckenby <leckenbym11@gmail.com>
2021-10-25 06:20:17 -0500

    complete

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const employee = {
-    name: 'Josh',
-    streetAddress: 'East 14th'
+let employee = {
+    name: "McKinsey",
+    streetAddress: "239 Valley Rd"
 }
-function updateEmployeeWithKeyAndValue(employee, key, value) {
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
     const newEmployee = {...employee};
     newEmployee[key] = value;
-    return newEmployee;
+    return newEmployee
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value
-    return employee;
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+    { employee[key] = value
+    return employee
 }
+
 function deleteFromEmployeeByKey(employee, key) {
     const newObject = Object.assign({}, employee)
     delete newObject[key]
@@ -20,3 +23,17 @@ function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key]
     return employee;
 }
+
+
+
+
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newObject = Object.assign({}, employee)
+//     delete newObject[key]
+//     return newObject;
+// }
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key]
+//     return employee;
+//}
